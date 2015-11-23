1b725d12677bc5695180ae11e311d2e40ff07b55
Chancey Ward <chancey_ward@yahoo.com>
2015-11-23 10:57:42 -0600

    slider works

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
 export default function() {
+  var btn = $(`.fa-plus-circle`);
+  var form = $(`.the-brain`);
 
-  };
+    $(`.fa-plus-circle`).on(`click`, function() {
+    $(`.the-brain`).slideToggle();
+    return false;
+  });
+
+
+  }
