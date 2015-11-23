d0f2a958f55991c1838dfb540e028eb35820ae62
Chancey Ward <chancey_ward@yahoo.com>
2015-11-23 13:35:08 -0600

    have server recieving and giving back info

--- src/app.js
+++ src/app.js
@@ -1,11 +1,50 @@
+ /* globals $ */
 export default function() {
   var btn = $(`.fa-plus-circle`);
   var form = $(`.the-brain`);
 
-    $(`.fa-plus-circle`).on(`click`, function() {
+  //to add li to the ul
+  var addPic = function(pics) {
+    var picItem = $('<li>').appendTo('.picture-list');
+
+    // Add an image to the new `picItem`
+    $('<img>')
+      .attr('src', pics.image)
+      .appendTo(picItem);
+
+    $('<p>').text(pics.caption).appendTo(picItem);
+
+    // Add an caption to the new `picItem`
+  };
+
+  btn.on(`click`, function() {
     $(`.the-brain`).slideToggle();
-    return false;
   });
 
+  form.on('submit', function(ev) {
+    ev.preventDefault();
 
-  }
+    var image = $(`#img-url`).val();
+    var caption = $(`#img-cap`).val();
+
+    $.ajax({
+      url: `http://tiny-lr.herokuapp.com/collections/photos-cw`,
+      method: `POST`,
+      dataType: `json`,
+      data: {image, caption},
+    }).then((response) => {
+      $(`.the-brain`).slideUp();
+      $(`#img-url`).val('');
+      $(`#img-cap`).val('');
+
+      // Take the response and use addPic to add it to the DOM
+      addPic(response);
+    });
+  });
+
+  $('.cancel').on('click', function() {
+    $('.the-brain').slideUp();
+    $('#img-url').val('');
+    $('#img-cap').val('');
+  });
+}
